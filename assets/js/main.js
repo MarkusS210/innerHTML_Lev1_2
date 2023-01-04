48757e7d38dda7a539158854763c150666c2749e
Markus <Markus.Schneiderm3@googlemail.com>
2023-01-04 13:30:55 +0100

    InnerHtmlLev1_2

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+let headerOne = document.getElementById ("haederOne")
+console.log(headerOne);
+
+document.write("<h1>Hello World</h1>")
+document.write("<h2>How are you?</h2>")
+
+let container = document.getElementById("container")
+console.log(container);
+
+container.innerHTML += "<p>start of the element</p>"
+
+let info = document.getElementById("info")
+console.log(info);
+
+document.write("<p>end of the element</p>")
